7c875cf335cabe4bac7bb8fb2ac698ce82f36720
alex_drum <alex_drum@mail.ru>
2020-06-19 20:41:19 +0800

    Initial commit

--- src/Lesson1/Fruits/Apple.java
+++
@@ -1,8 +0,0 @@
-package Lesson1.Fruits;
-
-public class Apple extends Fruit {
-
-    public Apple() {
-        super("apple", 1.0f);
-    }
-}
--- src/Lesson1/Fruits/Orange.java
+++
@@ -1,8 +0,0 @@
-package Lesson1.Fruits;
-
-public class Orange extends Fruit {
-
-    public Orange() {
-        super("orange", 1.5f);
-    }
-}
